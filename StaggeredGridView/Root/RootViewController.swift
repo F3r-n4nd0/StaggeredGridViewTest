14053dbd90827e02acfbfb10e826f52488a6c170
F3r-n4nd0 <337n4nd0@gmail.com>
2024-09-11 23:30:58 -0400

    base project

---
+++ StaggeredGridView/Root/RootViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+final class RootViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        // Additional setup
+    }
+}
+
